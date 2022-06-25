f9f016bc8131389d888c2b3182967c1e73dfc416
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-06-25 19:35:09 +0300

    + EditProductScreen ...1

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+
+  const EditProductScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Title'),
+                  textInputAction: TextInputAction.next,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
