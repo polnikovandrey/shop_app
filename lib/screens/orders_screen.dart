13f57d9cdd7435cdf37b1f8cbb62f81de5378622
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-06-26 20:31:36 +0300

    + FutureBuilder usage

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -14,31 +14,42 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  Future _ordersFuture = Future.value(null);
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<OrdersProvider>(context, listen: false).fetchAndSetOrders();
+  }
+
 
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() => _isLoading = true);
-      await Provider.of<OrdersProvider>(context, listen: false).fetchAndSetOrders();
-      setState(() => _isLoading = false);
-    });
+    _ordersFuture = _obtainOrdersFuture();
   }
 
   @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<OrdersProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading ? const Center(child: CircularProgressIndicator(),) : ListView.builder(
-        itemCount: ordersProvider.orders.length,
-        itemBuilder: (ctx, index) {
-          final orderItemData = ordersProvider.orders[index];
-          return OrderItem(orderItemData: orderItemData, key: ValueKey(orderItemData.id));
+      body: FutureBuilder(
+        future: _ordersFuture,
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (dataSnapshot.error != null) {
+            return const Center(child: Text('An error occurred'));
+          } else {
+            return Consumer<OrdersProvider>(builder: (ctx, ordersProvider, child) => ListView.builder(
+              itemCount: ordersProvider.orders.length,
+              itemBuilder: (ctx, index) {
+                final orderItemData = ordersProvider.orders[index];
+                return OrderItem(orderItemData: orderItemData, key: ValueKey(orderItemData.id));
+              },
+            ));
+          }
         },
       ),
     );
