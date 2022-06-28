cb001bbac340378a2b90900f5150a449b84d404c
Andrey Polnikov <polnikov.andrey@gmail.com>
2022-06-28 23:13:33 +0300

    + orders expanding animation

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -21,46 +21,56 @@ class _OrderItemState extends State<OrderItem> {
   @override
   Widget build(BuildContext context) {
     final products = widget._orderItemData.products;
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget._orderItemData.amount.toStringAsFixed(2)}'),
-            subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget._orderItemData.dateTime)),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () => setState(() => _expanded = !_expanded),
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(products.length * 20 + 10, 100),
-              child: ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (ctx, index) {
-                  var cartItemData = products[index];
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        cartItemData.title,
-                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        '${cartItemData.quantity} x \$${cartItemData.price}',
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  );
-                },
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      height: _expanded ? min(products.length * 20 + 110, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget._orderItemData.amount.toStringAsFixed(2)}'),
+              subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget._orderItemData.dateTime)),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () => setState(() => _expanded = !_expanded),
               ),
             ),
-        ],
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              child: Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                  height: _expanded ? min(products.length * 20 + 10, 100) : 0,
+                  child: LimitedBox(
+                    child: ListView.builder(
+                      itemCount: products.length,
+                      itemBuilder: (ctx, index) {
+                        var cartItemData = products[index];
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              cartItemData.title,
+                              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${cartItemData.quantity} x \$${cartItemData.price}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
